bf5d791dcc5fb5855dc978a0334268b211739719
Kasi <57307447+jhlee225@users.noreply.github.com>
2020-05-06 14:57:31 +0900

    complete ReadMe

--- routes/enterCar_UI.js
+++ routes/enterCar_UI.js
@@ -3,25 +3,25 @@ var router = express.Router();
 var mysql = require("mysql");
 
 /* GET */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("enterCar_UI", { title: "Express" });
 });
 
 /* POST */
-router.post("/", function(req, res) {
+router.post("/", function (req, res) {
   const connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "JH",
     password: "102938a",
     database: "parking_db",
-    dateStrings: "date"
+    dateStrings: "date",
   });
   connection.connect();
 
   connection.query(
     `insert into cars value(0,"${req.body.title}",now(),null);`,
-    function(err, rows, fields) {
+    function (err, rows, fields) {
       res.send("OK");
     }
   );
